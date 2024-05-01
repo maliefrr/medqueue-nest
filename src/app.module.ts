1f97f621be6cd58b25b20ee74ccf43c8d10d1fd5
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2024-05-01 13:24:50 +0700

    feat: add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PatientModule } from './patient/patient.module';
 import { AdminModule } from './admin/admin.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [UserModule, AuthModule, PatientModule, AdminModule],
+  imports: [UserModule, AuthModule, PatientModule, AdminModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
