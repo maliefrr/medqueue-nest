393681989fa8a2470069353b58bd76dba49d163e
Mohamad Alief Rizky Ramadhan <aliefm144@gmail.com>
2024-05-01 16:35:14 +0700

    fix: add registration user service but still error

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,58 @@
-import { Controller, Post } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Post } from '@nestjs/common';
+import {
+  AuthService,
+  RegistrationBody,
+  registerResponse,
+} from './auth.service';
+import moment from 'moment';
 
 @Controller('')
 export class AuthController {
+  constructor(private authService: AuthService) {}
+
   @Post('/register')
-  userRegister(): string {
-    return 'User Register Success';
+  async userRegister(
+    @Body() data: RegistrationBody,
+  ): Promise<registerResponse> {
+    if (
+      !data.email ||
+      !data.gender ||
+      !data.nama ||
+      !data.no_nik ||
+      !data.no_telepon ||
+      !data.password ||
+      !data.tempat_lahir ||
+      !data.tgl_lahir
+    ) {
+      throw new BadRequestException();
+    }
+    const role = 'PATIENT';
+    const tgl_lahir = moment(data.tgl_lahir).format('YYYY-MM-DD');
+
+    if (!moment(tgl_lahir, 'YYYY-MM-DD', true).isValid()) {
+      throw new BadRequestException('Invalid date');
+    }
+
+    await this.authService.register({
+      nama: data.nama,
+      email: data.email,
+      no_nik: data.no_nik,
+      gender: data.gender,
+      no_telepon: data.no_telepon,
+      tgl_lahir: new Date(tgl_lahir),
+      tempat_lahir: data.tempat_lahir,
+      gol_darah: data.gol_darah,
+      no_bpjs: data.no_bpjs,
+      password: data.password,
+      role: role,
+    });
+
+    return {
+      message: 'Data has been successfully added to the database',
+      data,
+    };
   }
+
   @Post('/register/admin')
   adminRegister(): string {
     return 'Admin Register Success';
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,48 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { hash, genSalt } from 'bcrypt';
+
+export interface RegistrationBody {
+  id?: string;
+  nama: string;
+  email: string;
+  password: string;
+  tempat_lahir: string;
+  tgl_lahir: Date;
+  no_bpjs?: string;
+  gender: string;
+  no_nik: string;
+  gol_darah?: string;
+  no_telepon: string;
+  role: 'ADMIN' | 'PATIENT';
+}
+
+export interface registerResponse {
+  message: string;
+  data: RegistrationBody;
+}
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prismaService: PrismaService) {}
+
+  async register(registerBody: RegistrationBody) {
+    const salt = await genSalt(10);
+    const hashedPassword = await hash(registerBody.password, salt);
+    await this.prismaService.user.create({
+      data: {
+        email: registerBody.email,
+        gender: registerBody.gender,
+        gol_darah: registerBody.gol_darah,
+        nama: registerBody.nama,
+        no_bpjs: registerBody.no_bpjs,
+        no_nik: registerBody.no_nik,
+        no_telepon: registerBody.no_telepon,
+        tempat_lahir: registerBody.tempat_lahir,
+        tgl_lahir: registerBody.tgl_lahir,
+        role: registerBody.role,
+        password: hashedPassword,
+      },
+    });
+  }
+}
